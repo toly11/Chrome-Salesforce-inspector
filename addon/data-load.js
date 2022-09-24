b4403fdefb9e10de6affb3e158af953b87b18d46
user <user@domain.com>
2022-09-24 21:15:03 +0300

    move copyToClipboard to utils folder

--- addon/data-load.js
+++ addon/data-load.js
@@ -1,5 +1,7 @@
 import {sfConn, apiVersion} from "./inspector.js";
 
+export {copyToClipboard} from "./utils/copyToClipboard.js";
+
 // Inspired by C# System.Linq.Enumerable
 export function Enumerable(iterable) {
   this[Symbol.iterator] = iterable[Symbol.iterator].bind(iterable);
@@ -121,35 +123,6 @@ export function DescribeInfo(spinFor, didUpdate) {
       didUpdate();
     }
   };
-}
-
-// Copy text to the clipboard, without rendering it, since rendering is slow.
-export function copyToClipboard(value) {
-  if (parent && parent.isUnitTest) { // for unit tests
-    parent.testClipboardValue = value;
-    return;
-  }
-  // Use execCommand to trigger an oncopy event and use an event handler to copy the text to the clipboard.
-  // The oncopy event only works on editable elements, e.g. an input field.
-  let temp = document.createElement("input");
-  // The oncopy event only works if there is something selected in the editable element.
-  temp.value = "temp";
-  temp.addEventListener("copy", e => {
-    e.clipboardData.setData("text/plain", value);
-    e.preventDefault();
-  });
-  document.body.appendChild(temp);
-  try {
-    // The oncopy event only works if there is something selected in the editable element.
-    temp.select();
-    // Trigger the oncopy event
-    let success = document.execCommand("copy");
-    if (!success) {
-      alert("Copy failed");
-    }
-  } finally {
-    document.body.removeChild(temp);
-  }
 }
 
 function renderCell(rt, cell, td) {
